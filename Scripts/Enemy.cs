ae112bc5ad2dea894fa78e7a29e6c9cd731fc490
clamascabaleiro <chrislamas1997@gmail.com>
2019-10-29 10:54:57 +0100

    Subo la carpeta scripts del roll a ball

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+
+	// Use this for initialization
+	public NavMeshAgent Enemigo;
+	//nuestro enemigo la capsula
+	public Transform goal;
+	//nuestro player es publica
+	public float proximidad;
+	//distancia minima para camibar la animacion
+	Animator animator;
+	//Creamos el animator
+
+
+	void Start () {
+		Enemigo = GetComponent<NavMeshAgent>();
+		Enemigo.destination = goal.position;
+		//asignamos el animator del player
+		animator = goal.GetComponent<Animator>();
+		proximidad = 5;
+	}
+
+	void Update() {
+		//actualizamos la posicion de la capsula con el goal
+		//con el player/target
+		Enemigo.destination = goal.position;
+		//comparamos la distancia a la capsula y cambiamos la variable bool para forzar la transicion
+		//de peligro a tranquilo.
+
+		if (!Enemigo.pathPending && Enemigo.remainingDistance < proximidad)
+		{
+			Debug.Log("Peligro");
+			//cambiamos la variable del animator
+			animator.SetBool("IsCerca", true);
+		}
+		else
+		{
+			Debug.Log("Tranquilo");
+			//cambiamos la variable del animator de nuevo para que cambie de transicion
+			animator.SetBool("IsCerca", false);
+		}
+	}
+	
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private Rigidbody player;
+    private int count;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Enter");
+    }
+    
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ( "Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+
+        if (other.gameObject.CompareTag( "Enemigo"))
+        {
+            countText.text = "Oughh!!!!!";
+            count = count - 3;
+            // vuelve al centro del tablero
+           player.transform.position = Vector3.one;
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+            countText.text = "You Win!";
+            Destroy(gameObject);
+            
+        }
+        
+    }
+}
